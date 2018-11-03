6429f4bb4797b018ba13d8d96f2e068291326918
DavidCoenFish <fish_dsc@yahoo.co.uk>
2018-11-03 15:48:06 +1100

    basic package work

--- web00/core/source/eventdispatcherdecorate.js
+++ web00/core/source/eventdispatcherdecorate.js
@@ -1,6 +1,5 @@
 module.exports = function(in_targetObject){
 	const eventMap = {};
-
 	Object.assign(in_targetObject, {
 		"addEventListener" : function(in_name, in_callback){
 			if (in_name in eventMap){
@@ -20,6 +19,7 @@ module.exports = function(in_targetObject){
 			}
 			return;
 		},
+		// if you can pass in a closure, why have the in_param, pass in the thing emitting the event?
 		"triggerEvent" : function(in_name, in_param){
 			if (in_name in eventMap){
 				const dataArray = eventMap[in_name];
