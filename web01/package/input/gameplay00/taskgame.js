8dc8f1415b35a852e364719ea85dca40837dfb8e
DavidCoenFish <fish_dsc@yahoo.co.uk>
2019-05-08 22:46:03 +1000

    game render skybox

--- web01/package/input/gameplay00/taskgame.js
+++ web01/package/input/gameplay00/taskgame.js
@@ -1,5 +1,7 @@
 import {factoryAppend as buttonFactory}  from './../manipulatedom/button.js';
 import {factoryFloat32 as Colour4FactoryFloat32} from "./../core/colour4.js";
+import RenderFactory from "./taskgame/render.js";
+
 
 /*
  */
@@ -15,14 +17,17 @@ export default function(in_webGLState, in_div, in_gameResourceManager, in_gameSt
 		"width": "32px", 
 		"height": "32px"
 	});
+	var m_render = RenderFactory(in_webGLState, in_gameResourceManager, in_gameState, in_callbackSetActiveGameTask, in_callbackRequestLoading);
 
 	const that = Object.create({
 		"destroy" : function(){
 			in_div.removeChild(m_button);
+			m_render.destroy();
 		},
 		"update" : function(in_timeDelta){
-			in_webGLState.clear(Colour4FactoryFloat32(0.0,0.0,1.0,1.0));
+			//in_webGLState.clear(Colour4FactoryFloat32(0.0,0.0,1.0,1.0));
 			//draw game world
+			m_render.update(in_timeDelta);
 			// input click drag camera, click on things in world (move, attack, talk) and status buttons
 		},
 	});
--- web01/package/input/gameplay00/taskgame/render.js
+++ web01/package/input/gameplay00/taskgame/render.js
@@ -1,16 +1,18 @@
 import ComponentCameraRayFactory from "./component-camera-ray.js";
 import ComponentSkyboxFactory from "./component-skybox.js";
+import {factoryFloat32 as Vector3FactoryFloat32} from "./../../core/vector3.js";
+import {factoryFloat32 as Matrix44FactoryFloat32} from "./../../core/matrix44.js";
 
 /*
  */
 
 export default function(in_webGLState, in_gameResourceManager, in_gameState, in_callbackSetActiveGameTask, in_callbackRequestLoading){
-	const m_fovHRadian = 120.0 / Math.PI;
+	const m_fovHRadian = 120.0 * (Math.PI/180.0);
 	const m_far = 100.0;
 
-	var m_cameraAt = Vector3FactoryFloat32();
-	var m_cameraLeft = Vector3FactoryFloat32();
-	var m_cameraUp = Vector3FactoryFloat32();
+	var m_cameraAt = Vector3FactoryFloat32(1.0, 0.0, 0.0);
+	var m_cameraLeft = Vector3FactoryFloat32(0.0, 1.0, 0.0);
+	var m_cameraUp = Vector3FactoryFloat32(0.0, 0.0, 1.0);
 	var m_cameraPos = Vector3FactoryFloat32();
 	var m_camera = Matrix44FactoryFloat32();
 
@@ -18,13 +20,13 @@ export default function(in_webGLState, in_gameResourceManager, in_gameState, in_
 	m_camera.set10(m_cameraAt.getY());
 	m_camera.set20(m_cameraAt.getZ());
 	m_camera.set30(m_fovHRadian);
-	m_camera.set01(in_state.u_cameraLeft[0]);
-	m_camera.set11(in_state.u_cameraLeft[1]);
-	m_camera.set21(in_state.u_cameraLeft[2]);
+	m_camera.set01(m_cameraLeft.getX());
+	m_camera.set11(m_cameraLeft.getY());
+	m_camera.set21(m_cameraLeft.getZ());
 	m_camera.set31(in_webGLState.getCanvasWidth());
-	m_camera.set02(in_state.u_cameraUp[0]);
-	m_camera.set12(in_state.u_cameraUp[1]);
-	m_camera.set22(in_state.u_cameraUp[2]);
+	m_camera.set02(m_cameraUp.getX());
+	m_camera.set12(m_cameraUp.getY());
+	m_camera.set22(m_cameraUp.getZ());
 	m_camera.set32(in_webGLState.getCanvasHeight());
 	m_camera.set03(m_cameraPos.getX());
 	m_camera.set13(m_cameraPos.getY());
@@ -34,7 +36,7 @@ export default function(in_webGLState, in_gameResourceManager, in_gameState, in_
 	var m_state = {
 		"u_camera" : m_camera.getRaw()
 	};
-	var m_componentCameraRay = ComponentCameraRayFactory(in_gameResourceManager.getResourceManager(), in_webGLState, fovHRadian, 
+	var m_componentCameraRay = ComponentCameraRayFactory(in_gameResourceManager.getResourceManager(), in_webGLState, m_fovHRadian, 
 		in_webGLState.getCanvasWidth(),
 		in_webGLState.getCanvasHeight()
 		);
@@ -42,13 +44,16 @@ export default function(in_webGLState, in_gameResourceManager, in_gameState, in_
 
 	const that = Object.create({
 		"destroy" : function(){
-			in_div.removeChild(m_button);
+			m_componentCameraRay.destroy();
+			m_componentSkybox.destroy();
 		},
 		"update" : function(in_timeDelta){
 			m_componentCameraRay.update(
 				in_webGLState.getCanvasWidth(),
 				in_webGLState.getCanvasHeight()
 			);
+
+			in_webGLState.applyRenderTarget();
 			m_componentSkybox.setTexture(m_componentCameraRay.getTexture());
 			m_componentSkybox.draw();
 		},
