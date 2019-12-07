bae13fb098be8ba43bc6725a47ff2cd7bb728d06
DavidCoenFish <fish_dsc@yahoo.co.uk>
2019-12-07 23:08:09 +1100

    something working

--- minmon00/prototype2/input/dom/canvasfactory.js
+++ minmon00/prototype2/input/dom/canvasfactory.js
@@ -1,6 +1,8 @@
 import applyStyle from './applystyle.js';
 
 export default function(in_document, in_canvasStyleDictionaryOrUndefined){
+	console.log("canvasfactory");
+
 	var m_element = in_document.createElement("CANVAS");
 
 	applyStyle(m_element, in_canvasStyleDictionaryOrUndefined);
@@ -9,27 +11,14 @@ export default function(in_document, in_canvasStyleDictionaryOrUndefined){
 	const that = Object.create({
 		"getElement" : function(){
 			return m_element;
+		},
+		"onResize" : function(){
+			console.log("onResize:" + m_element.offsetWidth + " " + m_element.offsetHeight);
+			m_element.width = m_element.offsetWidth;
+			m_element.height = m_element.offsetHeight;
+			return;
 		}
 	});
 
-	const resizeObserver = new ResizeObserver(entries => {
-		// for (let entry of entries) {
-		// 	console.log("entry:" + entry);
-		// }
-		// console.log("resizeObserver:" + m_element.offsetWidth + " " + m_element.offsetHeight);
-		m_element.width = m_element.offsetWidth;
-		m_element.height = m_element.offsetHeight;
-	});
-
-	resizeObserver.observe(m_element);	
-
 	return that;
 }
-
-// export const factoryAppendElement = function(in_document, in_parent, in_canvasStyleDictionaryOrUndefined){
-// 	var result = factory(in_document, in_canvasStyleDictionaryOrUndefined);
-// 	var element = result.getElement();
-// 	in_parent.appendChild(element);
-// 	result.onResize();
-// 	return result;
-// }
--- minmon00/prototype2/input/test/triangle.js
+++ minmon00/prototype2/input/test/triangle.js
@@ -2,20 +2,28 @@ import CanvasFactory from './../dom/canvasfactory.js'
 import WebGLAPIFactory from './../webgl/apifactory.js'
 
 export default function () {
-	const canvaseWrapper = CanvasFactory(document, {
+	const html5CanvasWrapper = CanvasFactory(document, 
+	{
 		"width" : "512px",
 		"height" : "512px",
-		"backgroundColor" : "#000000", //"#FFFFFF"
+		"backgroundColor" : "#FFFFFF",
 		"margin" : "0",
 		"padding" : "0",
 		"display" : "block"
-		});
-	document.body.appendChild(canvaseWrapper.getElement());
+	}
+	);
 
-	const webGLApi = WebGLAPIFactory(canvaseWrapper.getElement(), undefined, false);
+	console.log("pre appendChild");
+	document.body.appendChild(html5CanvasWrapper.getElement());
+	console.log("post appendChild");
+	html5CanvasWrapper.onResize();
 
-	const data = new Float32Array([1.0, 0.0, 0.0, 0.0]);
+	console.log("WebGLAPIFactory");
+	const webGLApi = WebGLAPIFactory(html5CanvasWrapper.getElement(), undefined, false);
 
+	const data = new Float32Array([0.0, 1.0, 0.0, 0.0]);
+
+	console.log("webGLApi clear");
 	webGLApi.clear(data[0], data[1], data[2], data[3]);
 	//webGLApi.clear(0.5);
 
