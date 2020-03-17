cd90b2f6927b463af2ac597d01b9a61b57f06ed1
DavidCoenFish <fish_dsc@yahoo.co.uk>
2020-03-17 21:03:34 +1100

    keyboard movement

--- unity/test00/Assets/CreatureComponent.cs
+++ unity/test00/Assets/CreatureComponent.cs
@@ -2,74 +2,20 @@
 public class CreatureComponent : UnityEngine.MonoBehaviour
 {
     public string typeName = "rat"; //[rat, chicken, 
-    struct SpherePosData
-    {
-        public UnityEngine.Vector3 center { get; set; }
-        public float radius { get; set; }
-    }
-    //private SpherePosData _test = new SpherePosData{ center = new UnityEngine.Vector3(0.0f, 0.0f, 0.0f ), radius = 1.0f };
-
-    private static SpherePosData[] _standingSphereCollider = {
-        new SpherePosData{ center = new UnityEngine.Vector3(0.0f, -0.25f, 0.0f ), radius = 0.25f },
-        new SpherePosData{ center = new UnityEngine.Vector3(0.0f, 0.25f, 0.0f ), radius = 0.25f },
-        new SpherePosData{ center = new UnityEngine.Vector3(0.25f, 0.375f, 0.0f ), radius = 0.125f }
-    }; 
-    //private SpherePosData[] _standingChildSphere = _standingSphereCollider with radius * 2.0
-
-    private static SpherePosData[] _crouchSphereCollider = {
-        new SpherePosData{ center = new UnityEngine.Vector3(0.0f, 0.0f, 0.125f ), radius = 0.25f },
-        new SpherePosData{ center = new UnityEngine.Vector3(0.0f, 0.0f, -0.125f ), radius = 0.25f },
-        new SpherePosData{ center = new UnityEngine.Vector3(0.25f, 0.175f, 0.0f ), radius = 0.125f }
-    }; 
-
-    struct ChildTransform
-    {
-        public UnityEngine.Vector3 position { get; set; }
-        public UnityEngine.Vector3 rotation { get; set; }
-        public UnityEngine.Vector3 scale { get; set; }
-    }
-
-    private static ChildTransform[] _standCubeMesh = {
-        new ChildTransform{ 
-            position = new UnityEngine.Vector3(0.338f, 0.486f, 0.054f ),
-            rotation = new UnityEngine.Vector3(-30.0f, 0.0f, 0.0f ),
-            scale = new UnityEngine.Vector3(0.03f, 0.03f, 0.1f )
-        },
-        new ChildTransform{ 
-            position = new UnityEngine.Vector3(0.338f, 0.486f, -0.054f ),
-            rotation = new UnityEngine.Vector3(30.0f, 0.0f, 0.0f ),
-            scale = new UnityEngine.Vector3(0.03f, 0.03f, 0.1f )
-        }
-    };
-
-    private static ChildTransform[] _crouchCubeMesh = {
-        new ChildTransform{ 
-            position = new UnityEngine.Vector3(0.35f, 0.2f, 0.054f ),
-            rotation = new UnityEngine.Vector3(-30.0f, 0.0f, 0.0f ),
-            scale = new UnityEngine.Vector3(0.03f, 0.03f, 0.1f )
-        },
-        new ChildTransform{ 
-            position = new UnityEngine.Vector3(0.35f, 0.2f, -0.054f ),
-            rotation = new UnityEngine.Vector3(30.0f, 0.0f, 0.0f ),
-            scale = new UnityEngine.Vector3(0.03f, 0.03f, 0.1f )
-        }
-    };
-
-    private UnityEngine.Rigidbody _rigidbody;
-    private System.Collections.Generic.List< UnityEngine.SphereCollider > _sphereColliderArray = new System.Collections.Generic.List< UnityEngine.SphereCollider >();
-    private System.Collections.Generic.List< UnityEngine.GameObject > _childGameObjectArraySphere = new System.Collections.Generic.List< UnityEngine.GameObject >();
-    private System.Collections.Generic.List< UnityEngine.GameObject > _childGameObjectArrayCube = new System.Collections.Generic.List< UnityEngine.GameObject >();
-    private float _height = 1.0f;
     private bool _touchingGround = false;
+    private CreatureExtraComponent _creatureExtraComponent = null;
+    private float _debugTimeAccumulate = 0.0f;
+    private UnityEngine.Rigidbody _rigidbody = null;
+    private float _height = 1.0f;
 
     struct TouchData
     {
-        public bool touchActive = false;
-        public UnityEngine.Vector2 touch = new UnityEngine.Vector2(0.0f, 0.0f);
-        public bool touchActiveOld = false;
-        public UnityEngine.Vector2 touchOld = new UnityEngine.Vector2(0.0f, 0.0f);
-        public float touchTimeAccumulation = 0.0f;
-        public bool doneJumpThisTouch = false;
+        public bool touchActive; // = false;
+        public UnityEngine.Vector2 touch; // = new UnityEngine.Vector2(0.0f, 0.0f);
+        public bool touchActiveOld; // = false;
+        public UnityEngine.Vector2 touchOld; // = new UnityEngine.Vector2(0.0f, 0.0f);
+        public float touchTimeAccumulation; // = 0.0f;
+        public bool doneJumpThisTouch; // = false;
     }
     private TouchData _inputTouchData = new TouchData();
     private TouchData _viewTouchData = new TouchData();
@@ -109,7 +55,7 @@ public class CreatureComponent : UnityEngine.MonoBehaviour
     {
         UnityEngine.Debug.Log("CreatureComponent.Start", this);
 
-        float _height = 0.125f;//get height "creatures." + typeName + ".height"; 
+        _height = 0.125f;//get height "creatures." + typeName + ".height"; 
         //get jumpHeight "creatures." + typeName + ".jump_height"; 
         //get maxMove "creatures." + typeName + ".max_move"; 
 
@@ -117,64 +63,41 @@ public class CreatureComponent : UnityEngine.MonoBehaviour
         _rigidbody.constraints = UnityEngine.RigidbodyConstraints.FreezeRotationX | UnityEngine.RigidbodyConstraints.FreezeRotationZ;
         _rigidbody.useGravity = true;
 
-        foreach (SpherePosData data in _standingSphereCollider)
-        {
-            UnityEngine.SphereCollider sphereCollider = gameObject.AddComponent<UnityEngine.SphereCollider>();
-            sphereCollider.center = new UnityEngine.Vector3(data.center.x * _height, data.center.y * _height, data.center.z * _height);
-            sphereCollider.radius = data.radius * _height;
-            _sphereColliderArray.Add(sphereCollider);
-        }
-
-        //Assets/Resources/material/CreatureSkin.mat
-        UnityEngine.Material materialSkin = UnityEngine.Resources.Load<UnityEngine.Material>("material/CreatureSkin");
-        //Assets/Resources/material/CreatureDark.mat
-        UnityEngine.Material materialDark = UnityEngine.Resources.Load<UnityEngine.Material>("material/CreatureDark");
-
-        foreach (SpherePosData data in _standingSphereCollider)
-        {
-            UnityEngine.GameObject childGameObject = UnityEngine.GameObject.CreatePrimitive(UnityEngine.PrimitiveType.Sphere);
-            childGameObject.GetComponent<UnityEngine.Renderer>().sharedMaterial = materialSkin;
-            childGameObject.transform.parent = gameObject.transform;
-            childGameObject.transform.localPosition = new UnityEngine.Vector3(data.center.x * _height, data.center.y * _height, data.center.z * _height);
-            float childScale = data.radius * _height * 2.0f;
-            childGameObject.transform.localScale = new UnityEngine.Vector3(childScale, childScale, childScale);
-            _childGameObjectArraySphere.Add(childGameObject);
-        }
-
-        foreach (ChildTransform data in _standCubeMesh)
-        {
-            UnityEngine.GameObject childGameObject = UnityEngine.GameObject.CreatePrimitive(UnityEngine.PrimitiveType.Cube);
-            childGameObject.GetComponent<UnityEngine.Renderer>().sharedMaterial = materialDark;
-            childGameObject.transform.parent = gameObject.transform;
-            childGameObject.transform.localPosition = new UnityEngine.Vector3(data.position.x * _height, data.position.y * _height, data.position.z * _height);
-            childGameObject.transform.localRotation = UnityEngine.Quaternion.Euler(data.rotation.x, data.rotation.y, data.rotation.z);
-            childGameObject.transform.localScale = new UnityEngine.Vector3(data.scale.x * _height, data.scale.y * _height, data.scale.z * _height);
-
-            _childGameObjectArrayCube.Add(childGameObject);
-        }
+        _creatureExtraComponent = gameObject.AddComponent<CreatureExtraComponent>();
+        _creatureExtraComponent.typeName = typeName;
+        _creatureExtraComponent.crouch = UnityEngine.Mathf.Sin(_debugTimeAccumulate * 0.5f);
+        _creatureExtraComponent.height = _height;
     }
 
+    private new UnityEngine.Vector3 _inputs = new UnityEngine.Vector3();
     void Update()
     {
         //Time.deltaTime
         int layerMask = 1 << 8;
-        UnityEngine.Vector3 touchingGroundPosition = new UnityEngine.Vector3(_childGameObjectArrayCube.transform.position.x, _childGameObjectArrayCube.transform.position.y - (0.55f * _height), _childGameObjectArrayCube.transform.position.z);
-        _touchingGround = UnityEngine.Physics.CheckSphere(touchingGroundPosition, 0.1f, layerMask, QueryTriggerInteraction.Ignore);
+        //todo: may need info on 
+        UnityEngine.Vector3 touchingGroundPosition = new UnityEngine.Vector3(gameObject.transform.position.x, gameObject.transform.position.y - (0.55f * _height), gameObject.transform.position.z);
+        _touchingGround = UnityEngine.Physics.CheckSphere(touchingGroundPosition, 0.1f, layerMask, UnityEngine.QueryTriggerInteraction.Ignore);
 
-        bool doJump = UpdateTouchData(_inputTouchData, Time.deltaTime);
+        bool doJump = UpdateTouchData(_inputTouchData, UnityEngine.Time.deltaTime);
 
+        _debugTimeAccumulate += UnityEngine.Time.deltaTime;
+        _creatureExtraComponent.crouch = UnityEngine.Mathf.Sin(_debugTimeAccumulate * 0.5f);
+
+        _inputs.x = UnityEngine.Input.GetAxis("Horizontal");
+        _inputs.z = UnityEngine.Input.GetAxis("Vertical");
     }
 
     private static bool UpdateTouchData(TouchData touchData, float deltaTime)
     {
 
 
-        touchData.touchActiveOld = touchData.touchActive;
+        //touchData.touchActiveOld = touchData.touchActive;
+        return false;
     }
 
     void FixedUpdate()
     {
         //_rigidbody.MoveRotation(_rigidbody.rotation * deltaRotation);
-        //_rigidbody.MovePosition(_body.position + _inputs * Speed * Time.fixedDeltaTime);
+        _rigidbody.MovePosition(_rigidbody.position + (_inputs * UnityEngine.Time.fixedDeltaTime));
     }
 }
---
+++ unity/test00/Assets/CreatureExtraComponent.cs
@@ -0,0 +1,124 @@
+﻿public class CreatureExtraComponent : UnityEngine.MonoBehaviour
+{
+    public string typeName = "rat"; //[rat, chicken, 
+    public float crouch; // 0.0 => stand, 1.0 => crouch
+    public float height;
+
+    struct SpherePosData
+    {
+        public UnityEngine.Vector3 center { get; set; }
+        public float radius { get; set; }
+    }
+    //private SpherePosData _test = new SpherePosData{ center = new UnityEngine.Vector3(0.0f, 0.0f, 0.0f ), radius = 1.0f };
+
+    private static SpherePosData[] _standingSphereCollider = {
+        new SpherePosData{ center = new UnityEngine.Vector3(0.0f, -0.25f, 0.0f ), radius = 0.25f },
+        new SpherePosData{ center = new UnityEngine.Vector3(0.0f, 0.25f, 0.0f ), radius = 0.25f },
+        new SpherePosData{ center = new UnityEngine.Vector3(0.25f, 0.375f, 0.0f ), radius = 0.125f }
+    }; 
+    //private SpherePosData[] _standingChildSphere = _standingSphereCollider with radius * 2.0
+
+    private static SpherePosData[] _crouchSphereCollider = {
+        new SpherePosData{ center = new UnityEngine.Vector3(0.0f, 0.0f, 0.125f ), radius = 0.25f },
+        new SpherePosData{ center = new UnityEngine.Vector3(0.0f, 0.0f, -0.125f ), radius = 0.25f },
+        new SpherePosData{ center = new UnityEngine.Vector3(0.25f, 0.175f, 0.0f ), radius = 0.125f }
+    }; 
+
+    struct ChildTransform
+    {
+        public UnityEngine.Vector3 position { get; set; }
+        public UnityEngine.Vector3 rotation { get; set; }
+        public UnityEngine.Vector3 scale { get; set; }
+    }
+
+    private static ChildTransform[] _standCubeMesh = {
+        new ChildTransform{ 
+            position = new UnityEngine.Vector3(0.338f, 0.486f, 0.054f ),
+            rotation = new UnityEngine.Vector3(-30.0f, 0.0f, 0.0f ),
+            scale = new UnityEngine.Vector3(0.03f, 0.03f, 0.1f )
+        },
+        new ChildTransform{ 
+            position = new UnityEngine.Vector3(0.338f, 0.486f, -0.054f ),
+            rotation = new UnityEngine.Vector3(30.0f, 0.0f, 0.0f ),
+            scale = new UnityEngine.Vector3(0.03f, 0.03f, 0.1f )
+        }
+    };
+
+    private static ChildTransform[] _crouchCubeMesh = {
+        new ChildTransform{ 
+            position = new UnityEngine.Vector3(0.35f, 0.2f, 0.054f ),
+            rotation = new UnityEngine.Vector3(-30.0f, 0.0f, 0.0f ),
+            scale = new UnityEngine.Vector3(0.03f, 0.03f, 0.1f )
+        },
+        new ChildTransform{ 
+            position = new UnityEngine.Vector3(0.35f, 0.2f, -0.054f ),
+            rotation = new UnityEngine.Vector3(30.0f, 0.0f, 0.0f ),
+            scale = new UnityEngine.Vector3(0.03f, 0.03f, 0.1f )
+        }
+    };
+
+    private System.Collections.Generic.List< UnityEngine.GameObject > _childGameObjectArraySphere = new System.Collections.Generic.List< UnityEngine.GameObject >();
+    private System.Collections.Generic.List< UnityEngine.GameObject > _childGameObjectArrayCube = new System.Collections.Generic.List< UnityEngine.GameObject >();
+
+    static UnityEngine.Vector3 CreatureLerp(UnityEngine.Vector3 lhs, UnityEngine.Vector3 rhs, float ratio, float scale)
+    {
+        return new UnityEngine.Vector3(
+            ((lhs.x * (1.0f - ratio)) + (rhs.x * ratio)) * scale,
+            ((lhs.y * (1.0f - ratio)) + (rhs.y * ratio)) * scale,
+            ((lhs.z * (1.0f - ratio)) + (rhs.z * ratio)) * scale
+            );
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Debug.Log("CreatureExtraComponent.Start", this);
+
+        //Assets/Resources/material/CreatureSkin.mat
+        UnityEngine.Material materialSkin = UnityEngine.Resources.Load<UnityEngine.Material>("material/CreatureSkin");
+        //Assets/Resources/material/CreatureDark.mat
+        UnityEngine.Material materialDark = UnityEngine.Resources.Load<UnityEngine.Material>("material/CreatureDark");
+
+        foreach (SpherePosData data in _standingSphereCollider)
+        {
+            UnityEngine.GameObject childGameObject = UnityEngine.GameObject.CreatePrimitive(UnityEngine.PrimitiveType.Sphere);
+            childGameObject.GetComponent<UnityEngine.Renderer>().sharedMaterial = materialSkin;
+            childGameObject.transform.parent = gameObject.transform;
+            childGameObject.transform.localPosition = new UnityEngine.Vector3(data.center.x * height, data.center.y * height, data.center.z * height);
+            float childScale = data.radius * height * 2.0f;
+            childGameObject.transform.localScale = new UnityEngine.Vector3(childScale, childScale, childScale);
+            _childGameObjectArraySphere.Add(childGameObject);
+        }
+
+        foreach (ChildTransform data in _standCubeMesh)
+        {
+            UnityEngine.GameObject childGameObject = UnityEngine.GameObject.CreatePrimitive(UnityEngine.PrimitiveType.Cube);
+            childGameObject.GetComponent<UnityEngine.Renderer>().sharedMaterial = materialDark;
+            childGameObject.transform.parent = gameObject.transform;
+            childGameObject.transform.localPosition = new UnityEngine.Vector3(data.position.x * height, data.position.y * height, data.position.z * height);
+            childGameObject.transform.localRotation = UnityEngine.Quaternion.Euler(data.rotation.x, data.rotation.y, data.rotation.z);
+            childGameObject.transform.localScale = new UnityEngine.Vector3(data.scale.x * height, data.scale.y * height, data.scale.z * height);
+
+            _childGameObjectArrayCube.Add(childGameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //for (int index = 0; index < _childGameObjectArraySphere.Count; index++)
+        //{
+        //    UnityEngine.Vector3 position = CreatureLerp(_standingSphereCollider[index].center, _crouchSphereCollider[index].center, crouch, height);
+
+        //    UnityEngine.GameObject childGameObject = _childGameObjectArraySphere[index];
+        //    childGameObject.transform.localPosition = position;
+        //}
+
+        //for (int index = 0; index < _childGameObjectArrayCube.Count; index++)
+        //{
+        //    UnityEngine.GameObject childGameObject = _childGameObjectArrayCube[index];
+        //    childGameObject.transform.localPosition = CreatureLerp(_standCubeMesh[index].position, _crouchCubeMesh[index].position, crouch, height);
+        //}
+    }
+}
